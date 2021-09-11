eaeaae3b22288ba35b374c86874fdbd7bec78372
Jose Pagan <josepagan@pop-os.localdomain>
2021-09-11 08:41:00 +0100

    chore: cleaning outdated comments

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@
 /* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
 
 import { React, useEffect } from 'react';
-// import Container from 'react-bootstrap/Container'
-// import Button from 'react-bootstrap/Button';
-// import Navbar from 'react-bootstrap/Navbar'
-// import Nav from 'react-bootstrap/Nav'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { Link, Route, Switch, useHistory, Redirect } from 'react-router-dom';
-// import { LinkContainer } from 'react-router-bootstrap';
 import { init } from './store/utils';
 import MainContainer from './components/layout/MainContainer'
-// import LoginForm from './components/LoginForm';
-// import PrivateRoute from './components/routing/PrivateRoute';
 import NavBarMain from './components/layout/NavBarMain';
-
-const selectShit = state => state
 
 function App() {
  
   useEffect(() => {
     init()
   }, [])
-  // if (!authedUser) return null
 
   return (
     <>
